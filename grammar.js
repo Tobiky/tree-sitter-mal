93d14b02d079aaf3610ad1420d81fd7c76b61f68
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-02-12 10:25:06 +0100

    feat: grammar transfer from maltoolbox (#13)
    * feat: Whitespace and comments
    
    * fix: Using line comment to cut of block comment end is not errenous
    
    Block comment start cut off likely not errenous because there are no
    other productions.
    
    * test: Use global declaration rule/node in include decls
    
    * fix: Tests only contain titles, remove errenous body
    
    * feat: include_declaration, string, and partial declaration node/rules
    
    * fix: Remove body from test title
    
    * fix: Include comment in parse tree for correct parser semantics
    
    Without the comments being included in the parse tree, the generated
    parser fails to take precidence over other constructs in the parse
    trees. For example a string in a comment is still parsed as a string
    (without this change).
    
    * test: Use global declaration rule/node in category decls
    
    * feat: Grammar for empty category decls without meta
    
    * feat: meta tag production rule
    
    * test: Unroll meta list as thats what TS does
    
    * feat: Meta tag list in category decls
    
    * fix: Comments should not include child comments
    
    * fix: Remove bodies from test titles
    
    * test: Use global declaration rule/node in define decls
    
    * feat: Define Declaration grammar
    
    * fix: Remove body from test title
    
    * feat: Add global declaration node to associations decl
    
    * feat: associations declaration, association, and multiplicity grammar
    
    * test: Update for adjustments in association grammar
    
    * test: Use global declaration rule/node in tests for asset decls
    
    * fix: Unroll asset, meta, and variable lists
    
    * fix: Add missing identity field to test
    
    * feat: Empty asset declaration grammar
    
    * test: Move variable tests to seperate file
    
    * fix: Unroll lists
    
    TS does not use lists but repeated fields/values.
    
    * fix: Include global declaration node in attack step tests
    
    * feat: Empty attack-step with step type
    
    * test: Update to adjust for grammar
    
    * feat: Attack-step with tag grammar
    
    * feat: Attack-step with CIAs
    
    * test: Update to adjust for grammar
    
    * feat: TTC grammar for attack-steps
    
    * feat: Update tests to adjust for grammar
    
    * fix: Incorrectly balanced parentheses
    
    * feat: Meta grammar for Attack-step
    
    * test: Add precondition as a seperate node
    
    * fix: Remove extra ending comma
    
    * feat: Asset expressions for preconditions and reaching
    
    * misc: Adjustments from grammar
    
    * misc: Adjust reaching asset test based on grammar
    
    The grammar generalizes the expression into one node, where the operator gets a special field instead.
    
    * misc: Rename precondition node/field into plural preconditions
    
    * misc: Rename asset_expr to asset_expr_primary
    
    * feat: Let-variable declarations in assets
    
    * test: Minimal detector with two context assets
    
    * test: Detector with name
    
    * fix: Extra closing parentheses for assert values in tests
    
    * test: Detector with detector type
    
    * test: Detector with TTC expression
    
    Complex TTC expressions are tested in attack-step, so it is not needed
    here - just that TTC expressions work there.
    
    * test: Ensure commented bangs are parsed as detectors
    
    * fix: Incorrectly formatted test titles
    
    * feat: Detector with only context list grammar
    
    * fix: Add type identity to detector type test
    
    * feat: Add utility for any kind of token seperated rule
    
    * feat: Detector with name grammar
    
    * fix: Remove extra detector context asset
    
    * feat: Detector with detector type grammar
    
    * feat: Detector with TTC expression grammar
    
    * feat: Create common TTC root node
    
    * fix: Workflow using absolute path instead of relative path
    
    * misc: Merge main
    
    * fix!: Remove errenous up-to-date parser check in GH CI
    
    The action, verify-files-changed, reports false positives for changed files and is so unusable.

--- grammar.js
+++ grammar.js
@@ -10,8 +10,314 @@
 module.exports = grammar({
   name: "mal",
 
+  extras: $ => [
+    /[ \t\r\n]+/,
+    $.comment,
+  ],
+
+  word: $ => $.identity,
+
+  conflicts: $ => [
+    // FIXME: Conflict at end of association when there shouldn't be one
+    [$.association]
+  ],
+
+  precedences: $ => [
+    [ 'binary_exp', 'binary_mul', 'binary_plus', ]
+  ],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
-  }
+    source_file: $ => repeat($.declaration),
+
+    // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
+    // Additionally, set 0 precidence so behavior can be overwritten when necessary.
+    // For example: Detectors have //! accepted as part of backwards compatability.
+    comment: _ => token(prec(0, choice(
+      seq('//', /[^\r\n\u2028\u2029]*/),
+      seq(
+        '/*',
+        /[^*]*\*+([^/*][^*]*\*+)*/,
+        '/',
+      ),
+    ))),
+
+    declaration: $ => choice(
+      $.include_declaration,
+      $.category_declaration,
+      $.define_declaration,
+      $.associations_declaration,
+    ),
+
+    include_declaration: $ => seq(
+      'include',
+      field('file', $.string)
+    ),
+
+    category_declaration: $ => seq(
+      'category',
+      field('id', $.identity),
+      field('meta', repeat($.meta)),
+      '{',
+      field('assets', repeat($.asset)),
+      '}',
+    ),
+
+    asset: $ => seq(
+      optional(alias('abstract', 'abstract')),
+      'asset',
+      field('id', $.identity),
+      field('extends', optional(seq('extends', $.identity))),
+      field('meta', repeat($.meta)),
+      '{',
+      field('body', repeat(choice($.attack_step, $.asset_variable))),
+      '}',
+    ),
+
+    asset_variable: $ => seq(
+      'let',
+      field('id', $.identity),
+      '=',
+      field('value', $._asset_expr),
+    ),
+
+    attack_step: $ => seq(
+      field('step_type', choice(
+        '|',
+        '&',
+        '#',
+        'E',
+        '!E',
+      )),
+      field('id', $.identity),
+      optional(field('tag', seq('@', $.identity))),
+      optional(field('cias', seq(
+        '{',
+        commaSep1($.cia),
+        '}',
+      ))),
+      optional(field('ttc', $.ttc)),
+      field('meta', repeat($.meta)),
+      optional(field('detector', $.detector)),
+      optional(field('preconditions', $.preconditions)),
+      optional(field('reaches', $.reaching)),
+    ),
+
+    detector: $ => seq(
+      // Increase //! precidence to overrule comments
+      choice('!', token(prec(1, '//!'))),
+      optional(field('name', $.detector_name)),
+      field('context', $.detector_context),
+      optional(field('type', $.identity)),
+      optional(field('ttc', $.ttc)),
+    ),
+
+    detector_name: $ => sep1($.identity, '.'),
+
+    detector_context: $ => seq(
+      '(',
+      commaSep1($.detector_context_asset),
+      ')',
+    ),
+
+    detector_context_asset: $ => seq(
+      field('id', $.identity),
+      field('label', $.identity),
+    ),
+
+    preconditions: $ => seq(
+      '<-', 
+      field('condition', commaSep1($._asset_expr))
+    ),
+
+    reaching: $ => seq(
+        field('operator', choice('+>', '->')),
+        field('reaches', commaSep1($._asset_expr))
+    ),
+
+
+    ttc: $ => seq(
+      '[',
+      $._ttc_expr,
+      ']',
+    ),
+
+    // No use in being known since there is only one place these can occur.
+    // Might want to bring forward for the sake of querrying.
+    _ttc_expr: $ => choice(
+      $._ttc_parenthesized,
+      $.ttc_primary,
+      $.ttc_binop,
+    ),
+
+    _ttc_parenthesized: $ => seq('(', $._ttc_expr, ')'),
+
+    ttc_primary: $ => choice(
+      $.integer,
+      $.identity,
+      $.ttc_distribution,
+    ),
+
+    ttc_distribution: $ => seq(
+      field('id', $.identity),
+      '(',
+      field('values', commaSep1($.integer)),
+      ')',
+    ),
+
+    ttc_binop: $ => choice(
+      ...[
+        ['+', 'binary_plus'],
+        ['-', 'binary_plus'],
+        ['*', 'binary_mul'],
+        ['/', 'binary_mul'],
+        ['^', 'binary_exp', 'right'],
+      ].map(([operator, precedence, associativity]) =>
+        (associativity === 'right' ? prec.right : prec.left)(precedence, seq(
+          field('left', $._ttc_expr),
+          field('operator', operator),
+          field('right', $._ttc_expr),
+        )),
+      )
+    ),
+
+    _asset_expr: $ => choice(
+      $._asset_expr_parenthesized,
+      $.asset_expr_primary,
+      $.asset_expr_binop,
+      $.asset_expr_unop,
+      $.asset_expr_type,
+    ),
+
+    _asset_expr_parenthesized: $ => seq(
+      '(',
+      $._asset_expr,
+      ')',
+    ),
+
+    asset_expr_primary: $ => choice(
+      $.identity,
+      $.asset_variable_substitution
+    ),
+
+    asset_variable_substitution: $ => seq(
+      field('id', $.identity),
+      '(',
+      ')',
+    ),
+
+    asset_expr_type: $ => prec.left('binary_exp', seq(
+      field('expression', $._asset_expr),
+      '[',
+      field('type_id', $.identity),
+      ']',
+    )),
+
+    asset_expr_binop: $ => choice(
+      ...[
+        ['\\/', 'binary_plus'],
+        ['/\\', 'binary_mul'],
+        ['-', 'binary_mul'],
+        ['.', 'binary_exp'],
+      ].map(([operator, precedence, associativity]) =>
+        (associativity === 'right' ? prec.right : prec.left)(precedence, seq(
+          field('left', $._asset_expr),
+          field('operator', operator),
+          field('right', $._asset_expr),
+        )),
+      )
+    ),
+
+    asset_expr_unop: $ => choice(
+      ...[
+        // For now only one unary operator so use binary precedences
+        ['*', 'binary_exp'],
+      ].map(([operator, precedence, associativity]) =>
+        (associativity === 'right' ? prec.right : prec.left)(precedence, seq(
+          field('expression', $._asset_expr),
+          field('operator', operator),
+        )),
+      )
+    ),
+
+    define_declaration: $ => seq(
+      '#',
+      field('id', $.identity),
+      ':',
+      field('value', $.string)
+    ),
+
+    associations_declaration: $ => seq(
+      'associations',
+      '{',
+      repeat($.association),
+      '}',
+    ),
+
+    association: $ => seq(
+      field('left_id', $.identity),
+      '[', field('left_field_id', $.identity), ']',
+      field('left_mult', $.multiplicity),
+      '<--',
+      field('id', $.identity),
+      '-->',
+      field('right_mult', $.multiplicity),
+      '[', field('right_field_id', $.identity), ']',
+      field('right_id', $.identity),
+      field('meta', repeat($.meta)),
+    ),
+
+    multiplicity: $ => choice(
+      $._multiplicity_atom,
+      $.multiplicity_range,
+    ),
+
+    _multiplicity_atom: $ => choice(
+      $.integer,
+      $.star,
+    ),
+
+    multiplicity_range: $ => seq(
+      field('start', $._multiplicity_atom),
+      '..',
+      field('end', $._multiplicity_atom),
+    ),
+
+
+    meta: $ => seq(
+      field('id', $.identity),
+      'info',
+      ':',
+      field('info', $.string),
+    ),
+
+    string: _ => token(seq('"', /(?:\\"|[^"])*/, '"')),
+    integer: _ => token(/[0-9]+/),
+    identity: _ => token(/[a-zA-Z0-9_]+/),
+
+    star: _ => token('*'),
+    cia: _ => token(/[CIA]/)
+  },
 });
+
+/**
+ * Creates a rule to match one or more of the rules separated by a given token.
+ *
+ * @param {Rule} rule
+ * @param {Token} token
+ *
+ * @returns {SeqRule}
+ */
+function sep1(rule, token) {
+  return seq(rule, repeat(seq(token, rule)));
+}
+
+/**
+ * Creates a rule to match one or more of the rules separated by a comma
+ *
+ * @param {Rule} rule
+ *
+ * @returns {SeqRule}
+ */
+function commaSep1(rule) {
+  return sep1(rule, ',');
+}
