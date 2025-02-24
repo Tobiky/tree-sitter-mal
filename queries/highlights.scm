de0cf43c62679830b377ae1944d223519e501266
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-02-24 17:26:10 +0100

    feat: Operator syntax tagging

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -8,3 +8,31 @@
  "]"
 ] @punctuation.bracket
 
+; Operators
+[
+ "+"
+ "-"
+ "*"
+ "/"
+ "^"
+ "\\/"
+ "/\\"
+ ".."
+ "<--"
+ "-->"
+ "+>"
+ "->"
+ "<-"
+ "@"
+ "!"
+ "//!"
+ "|"
+ "&"
+ "#"
+ "E"
+ "!E"
+] @operator
+
+; To ensure that they are not interpreted as operators elsewhere (e.g. detector names)
+(asset_expr_binop
+  "." @operator)
