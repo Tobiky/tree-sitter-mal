31dd34cce0404ac6fd0c7528fe7989c51d61db63
sandorstormen <sandor.berglund@gmail.com>
2026-07-19 05:41:42 +0200

    Add me as author

--- grammar.js
+++ grammar.js
@@ -1,6 +1,7 @@
 /**
  * @file IT systems are growing in complexity and the threat from cyberattacks is increasing. Threat modeling is a process that can be used to analyze potential attacks to IT systems in order to facilitate secure design. Meta Attack Language (MAL) is a threat modeling language framework for the creation of domain specific languages (DSL). MAL is developed at KTH Royal Institute of Technology.
  * @author Andreas Hammarstrand <andreas.hammarstrand@gmail.com>
+ * @author Sandor Berglund <sandorb@kth.se>
  * @license MIT
  */
 
